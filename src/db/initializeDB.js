3b024b42aa70278f77dc3a1e835f41d2886edc33
Jake Zheng <momoko8443@163.com>
2023-09-14 09:25:25 +0800

    up

--- src/db/initializeDB.js
+++ src/db/initializeDB.js
@@ -1,9 +1,15 @@
 const userDAO = require('./dao/userDAO');
 const sequelize = require('./connect');
+const userService = require('../service/userService');
 
 async function dbPreCheck(){
     try{
         await sequelize.sync({force:false,alter:true});
+        const superAdmins = await userDAO.queryAll({status:1,role:'超级管理员'});
+        if(!superAdmins || superAdmins.length === 0){
+            //create first admin account
+            await userService.addUser('admin','P@ssw0rd123','超级管理员');
+        }
         return true;  
     }catch(err){
         console.error(err);
